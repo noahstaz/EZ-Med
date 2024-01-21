1af70756ad367bec1caa1b90a1e7f93d5e2b37c0
Noah <noah.stasuik@gmail.com>
2024-01-20 23:11:01 -0800

    reset

--- EZMedApp/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
-import Home from './home.js'; // Import your home.js file
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => Home); // Use Home as the registered component

0f858f2efdca27bc915874075fc4a5df07517fa9
Noah <noah.stasuik@gmail.com>
2024-01-21 10:27:56 -0800

    PatientUpdates page looking much better

--- ez-med/src/PatientUpdates.js
+++ ez-med/src/PatientUpdates.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import './PatientUpdates.css';
+
 function PatientUpdates() {
   const [patientId, setPatientId] = useState('65ac545aa8fd58d8744467e5');
   const [patientDetails, setPatientDetails] = useState(null);
   const [updates, setUpdates] = useState(null);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  // Define the fetchAndDisplayPatient function using useCallback
   const fetchAndDisplayPatient = useCallback(async () => {
+    setIsLoading(true);
     try {
       const patientResponse = await fetch(`http://localhost:8080/patients/${patientId}`);
       if (!patientResponse.ok) {
@@ -22,43 +24,45 @@ function PatientUpdates() {
       }
       const patientUpdates = await updatesResponse.json();
       setUpdates(patientUpdates);
-
-      setError(null);
     } catch (error) {
       console.error(error.message);
-      setError('Error fetching data');
-      setPatientDetails(null);
-      setUpdates(null);
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
     }
   }, [patientId]);
 
-  // Fetch data on component mount or when patientId changes
   useEffect(() => {
     fetchAndDisplayPatient();
   }, [fetchAndDisplayPatient]);
-    return (
-        <div>
-          {error && <div>Error: {error}</div>}
 
-          {patientDetails && (
-              <div className={"patient-updates-header"}>
-                <h1>Here are the updates for {patientDetails.name}</h1>
-              </div>
-          )}
+  return (
+    <div className="patient-updates-container">
+      {isLoading && <div className="loading">Loading...</div>}
+      {error && <div className="error-message">Error: {error}</div>}
 
-          {updates && (
-              <div >
-                {updates.length === 0 ? (
-                    <h2>No updates available for the patient</h2>
-                ) : (
-                    updates.map((update, index) => (
-                        <h2 key={index}>{update.date}: {update.update}</h2>
-                    ))
-                )}
-              </div>
+      {patientDetails && (
+        <div className="patient-updates-header">
+          <h1>Updates for {patientDetails.name}</h1>
+        </div>
+      )}
+
+      {updates && (
+        <div className="update-section">
+          {updates.length === 0 ? (
+            <h2>No updates available for the patient</h2>
+          ) : (
+            <ul>
+              {updates.map((update, index) => (
+                <li key={index}><strong>{update.date}:</strong> {update.update}</li>
+              ))}
+            </ul>
           )}
         </div>
-    );
+      )}
+    </div>
+  );
 }
+
 export default PatientUpdates;
 
